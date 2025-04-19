be8bcef99cd08bfa57d05755f942aad99f0ed642
Howon Lee <kck4156@kagamine-rin.com>
2025-04-19 12:59:16 +0900

    앱 제작중

---
+++ History.MobileClient/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace History.MobileClient
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ History.MobileClient/Social/IGoogleAuthService.cs
@@ -0,0 +1,15 @@
+﻿using History.MobileClient.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace History.MobileClient.Social.Interfaces;
+
+public interface IGoogleAuthService
+{
+    public Task<GoogleUserDTO> AuthenticateAsync();
+    public Task LogoutAsync();
+    public Task<GoogleUserDTO> GetCurrentUserAsync();
+}
