c744bc0cab83ae0148314159065a05055e7370ad
Howon Lee <kck4156@kagamine-rin.com>
2025-04-19 12:59:16 +0900

    앱 제작중

---
+++ History.MobileClient/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace History.MobileClient;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ History.MobileClient/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Mopups.Hosting;
+using UraniumUI;
+
+namespace History.MobileClient
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureMopups()
+                .UseUraniumUI()
+                .UseUraniumUIMaterial()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            builder.Services.AddMopupsDialogs();
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
