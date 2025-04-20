34d3bb2b96a81f6a5780cfece3029febab58c1dc
Howon Lee <kck4156@kagamine-rin.com>
2025-04-20 13:00:45 +0900

    데이터베이스 파라미터 오류 수정

--- History.AppHost/Program.cs
+++ History.AppHost/Program.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var username = builder.AddParameter("admin");
-var password = builder.AddParameter("***REMOVED***", secret: true);
+var username = builder.AddParameter("username", "admin");
+var password = builder.AddParameter("password", "***REMOVED***", secret: true);
 
 var mongodb = builder.AddMongoDB("MongoDB", 27017, username, password)
     .WithLifetime(ContainerLifetime.Persistent)
