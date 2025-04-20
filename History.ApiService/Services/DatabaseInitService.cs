c489e979e5444eabf9f8a78f732072cfe0d676d0
Howon Lee <kck4156@kagamine-rin.com>
2025-04-20 12:59:49 +0900

    데이터베이스 초기화 기능 추가

---
+++ History.ApiService/Services/DatabaseInitService.cs
@@ -0,0 +1,54 @@
+﻿
+using History.Commons.DataTypes;
+using MongoDB.Driver;
+
+namespace History.ApiService.Services
+{
+    public class DatabaseInitService(IMongoDatabase database, ILogger<DatabaseInitService> logger) : IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Initializing database...");
+
+            var userCollection = database.GetCollection<User>("Users");
+            var postCollection = database.GetCollection<Post>("Posts");
+            var friendshipCollection = database.GetCollection<Friendship>("Friendships");
+            var commentCollection = database.GetCollection<Comment>("Comments");
+            var mediaCollection = database.GetCollection<Media>("Medias");
+
+            // Create indexes
+            logger.LogInformation("Creating indexes...");
+
+            logger.LogInformation("Creating indexes for Users collection...");
+            await userCollection.Indexes.CreateOneAsync(new CreateIndexModel<User>(Builders<User>.IndexKeys.Ascending(x => x.Nickname)), cancellationToken: cancellationToken);
+
+            logger.LogInformation("Creating indexes for Posts collection...");
+            await postCollection.Indexes.CreateOneAsync(new CreateIndexModel<Post>(Builders<Post>.IndexKeys.Ascending(x => x.UserId)), cancellationToken: cancellationToken);
+            await postCollection.Indexes.CreateOneAsync(new CreateIndexModel<Post>(Builders<Post>.IndexKeys.Ascending(x => x.ParentPostId)), cancellationToken: cancellationToken);
+            await postCollection.Indexes.CreateOneAsync(new CreateIndexModel<Post>(Builders<Post>.IndexKeys.Ascending(x => x.SearchIndex)), cancellationToken: cancellationToken);
+            await postCollection.Indexes.CreateOneAsync(new CreateIndexModel<Post>(Builders<Post>.IndexKeys.Descending(x => x.CreatedAt)), cancellationToken: cancellationToken);
+
+            logger.LogInformation("Creating indexes for Friendships collection...");
+            await friendshipCollection.Indexes.CreateOneAsync(new CreateIndexModel<Friendship>(Builders<Friendship>.IndexKeys.Ascending(x => x.UserId)), cancellationToken: cancellationToken);
+            await friendshipCollection.Indexes.CreateOneAsync(new CreateIndexModel<Friendship>(Builders<Friendship>.IndexKeys.Ascending(x => x.FriendId)), cancellationToken: cancellationToken);
+            await friendshipCollection.Indexes.CreateOneAsync(new CreateIndexModel<Friendship>(Builders<Friendship>.IndexKeys.Ascending(x => x.Status)), cancellationToken: cancellationToken);
+            await friendshipCollection.Indexes.CreateOneAsync(new CreateIndexModel<Friendship>(Builders<Friendship>.IndexKeys.Ascending(x => x.CreatedAt)), cancellationToken: cancellationToken);
+
+            logger.LogInformation("Creating indexes for Comments collection...");
+            await commentCollection.Indexes.CreateOneAsync(new CreateIndexModel<Comment>(Builders<Comment>.IndexKeys.Ascending(x => x.PostId)), cancellationToken: cancellationToken);
+            await commentCollection.Indexes.CreateOneAsync(new CreateIndexModel<Comment>(Builders<Comment>.IndexKeys.Ascending(x => x.UserId)), cancellationToken: cancellationToken);
+            await commentCollection.Indexes.CreateOneAsync(new CreateIndexModel<Comment>(Builders<Comment>.IndexKeys.Descending(x => x.CreatedAt)), cancellationToken: cancellationToken);
+
+            logger.LogInformation("Creating indexes for Medias collection...");
+            await mediaCollection.Indexes.CreateOneAsync(new CreateIndexModel<Media>(Builders<Media>.IndexKeys.Ascending(x => x.FileName)), cancellationToken: cancellationToken);
+            await mediaCollection.Indexes.CreateOneAsync(new CreateIndexModel<Media>(Builders<Media>.IndexKeys.Ascending(x => x.UserId)), cancellationToken: cancellationToken);
+            await mediaCollection.Indexes.CreateOneAsync(new CreateIndexModel<Media>(Builders<Media>.IndexKeys.Ascending(x => x.AssociatedId)), cancellationToken: cancellationToken);
+            await mediaCollection.Indexes.CreateOneAsync(new CreateIndexModel<Media>(Builders<Media>.IndexKeys.Ascending(x => x.BucketType)), cancellationToken: cancellationToken);
+            await mediaCollection.Indexes.CreateOneAsync(new CreateIndexModel<Media>(Builders<Media>.IndexKeys.Ascending(x => x.CreatedAt)), cancellationToken: cancellationToken);
+
+            logger.LogInformation("Indexes created successfully.");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
