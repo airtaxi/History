8d9d12ae86a34d7d1c6fd1f3fe9dd68e4e9d37a1
Howon Lee <kck4156@kagamine-rin.com>
2025-04-20 11:31:55 +0900

    패스워드 수정

--- History.AppHost/Program.cs
+++ History.AppHost/Program.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var username = builder.AddParameter("admin");
-var password = builder.AddParameter("7742516665ff3ea9a9011c3645119f15baefc947d1a5ae09cdebc791d0148af2", secret: true);
+var password = builder.AddParameter("a742516665ff3ea9a9011c3645119f15baefc947d1a5ae09cdebc791d0148af2", secret: true);
 
 var mongodb = builder.AddMongoDB("MongoDB", 27017, username, password)
     .WithLifetime(ContainerLifetime.Persistent)
@@ -10,7 +10,6 @@ var mongodb = builder.AddMongoDB("MongoDB", 27017, username, password)
 
 builder.AddProject<Projects.History_ApiService>("ApiService")
     .WithReference(mongodb)
-    .WithHttpEndpoint()
     .WaitFor(mongodb);
 
 builder.Build().Run();
