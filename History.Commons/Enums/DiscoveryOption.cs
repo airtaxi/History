d3b993ac8275b58767ac9849a029036152426418
Howon Lee <kck4156@kagamine-rin.com>
2025-04-23 11:40:16 +0900

    특정 친구 비공개 옵션 추가 및 익스텐션 클래스 작성

--- History.Commons/Enums/DiscoveryOption.cs
+++ History.Commons/Enums/DiscoveryOption.cs
@@ -7,7 +7,39 @@ public enum DiscoveryOption
 {
     OnlyMe,
     SelectedUsers,
+    UnselectedUsers,
     Friends,
     FriendsOfFriends,
     Everyone,
+}
+
+public static class DiscoveryOptionExtensions
+{
+    public static string ToDisplayString(this DiscoveryOption option)
+    {
+        return option switch
+        {
+            DiscoveryOption.OnlyMe => "나만 보기",
+            DiscoveryOption.SelectedUsers => "특정 친구 공개",
+            DiscoveryOption.UnselectedUsers => "특정 친구 비공개",
+            DiscoveryOption.Friends => "친구 공개",
+            DiscoveryOption.FriendsOfFriends => "친구의 친구 공개",
+            DiscoveryOption.Everyone => "전체 공개",
+            _ => throw new ArgumentOutOfRangeException(nameof(option), option, null)
+        };
+    }
+
+    public static DiscoveryOption FromDisplayString(string displayString)
+    {
+        return displayString switch
+        {
+            "나만 보기" => DiscoveryOption.OnlyMe,
+            "특정 친구 공개" => DiscoveryOption.SelectedUsers,
+            "특정 친구 비공개" => DiscoveryOption.UnselectedUsers,
+            "친구 공개" => DiscoveryOption.Friends,
+            "친구의 친구 공개" => DiscoveryOption.FriendsOfFriends,
+            "전체 공개" => DiscoveryOption.Everyone,
+            _ => throw new ArgumentOutOfRangeException(nameof(displayString), displayString, null)
+        };
+    }
 }
