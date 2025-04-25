f07e13efe614245b6f106493a6601cc0eefb1daf
Howon Lee <kck4156@kagamine-rin.com>
2025-04-25 20:10:06 +0900

    DeleteBackground/ProfleMedia 추가

---
+++ History.Commons/Api/User/DeleteProfileMedia.cs
@@ -0,0 +1,17 @@
+﻿using History.Commons.DataTypes.RequestDtos;
+using History.Commons.DataTypes.ResponseDtos;
+using History.Commons.Interfaces;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace History.Commons.Api.User;
+
+public class DeleteProfileMedia : IAuthRequiredRequest
+{
+    public string Path => "/api/user/profile-media";
+    public Method Method => Method.Delete;
+}
