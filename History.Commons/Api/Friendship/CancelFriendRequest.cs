86f921e1e04b37d31d2a049e1af360f7bb4afb57
Howon Lee <kck4156@kagamine-rin.com>
2025-04-27 17:40:51 +0900

    CancelFriendRequest API 추가

---
+++ History.Commons/Api/Friendship/CancelFriendRequest.cs
@@ -0,0 +1,18 @@
+﻿using History.Commons.Interfaces;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace History.Commons.Api.Friendship;
+
+public class CancelFriendRequest : IAuthRequiredRequest, IRequestWithUrlParameters
+{
+    public string Path => "/api/friendship/request/{userIdToCancel}/cancel";
+    public Method Method => Method.Post;
+    public Dictionary<string, string> UrlParameters { get; set; } = [];
+
+    public CancelFriendRequest(string userIdToCancel) => UrlParameters["userIdToCancel"] = userIdToCancel;
+}
