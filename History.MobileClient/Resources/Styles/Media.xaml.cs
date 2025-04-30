683b736e75d251362b942ad8099f94ae3fadaf6a
Howon Lee <kck4156@kagamine-rin.com>
2025-04-30 20:26:17 +0900

    비디오 램누수 수정

--- History.MobileClient/Resources/Styles/Media.xaml.cs
+++ History.MobileClient/Resources/Styles/Media.xaml.cs
@@ -1,9 +1,51 @@
+using CommunityToolkit.Maui.Core.Handlers;
+using CommunityToolkit.Maui.Views;
+using History.MobileClient.ViewModels;
+using Microsoft.Maui.Controls;
+using System.Collections.Concurrent;
+
 namespace History.MobileClient.Resources.Styles;
 
 public partial class Media : ResourceDictionary
 {
+    private static readonly ConcurrentDictionary<ContentView, IViewHandler> MediaElementHandlerMap = [];
 	public Media()
 	{
 		InitializeComponent();
-	}
+
+		var element = new MediaElement();
+		element.Dispose();
+
+    }
+
+    private void OnVideoContentViewLoaded(object sender, EventArgs e)
+    {
+        var contentView = sender as ContentView;
+        var mediaElement = new MediaElement();
+        contentView.Content = mediaElement;
+        var handler = mediaElement.Handler;
+        MediaElementHandlerMap[contentView] = handler;
+
+        var viewModel = contentView.BindingContext as VideoViewModel;
+        mediaElement.Aspect = viewModel.Aspect;
+        mediaElement.HorizontalOptions = viewModel.HorizontalContentOptions;
+        mediaElement.VerticalOptions = viewModel.VerticalContentOptions;
+        mediaElement.ShouldAutoPlay = viewModel.VideoShouldAutoPlay;
+        mediaElement.ShouldLoopPlayback = viewModel.VideoShouldLoopPlayback;
+        mediaElement.ShouldMute = contentView.BindingContext is not FullScreenVideoViewModel;
+        mediaElement.ShouldShowPlaybackControls = viewModel.VideoShouldShowPlaybackControls;
+        mediaElement.ShouldKeepScreenOn = false;
+        mediaElement.Source = MediaSource.FromUri(viewModel.Uri);
+    }
+
+    private void OnVideoContentViewUnloaded(object sender, EventArgs e)
+    {
+        var contentView = sender as ContentView;
+        if (MediaElementHandlerMap.TryGetValue(contentView, out var handler))
+        {
+            try { handler.DisconnectHandler(); }
+            catch (ObjectDisposedException) { }
+            finally { MediaElementHandlerMap.TryRemove(contentView, out var _); }
+        }
+    }
 }
