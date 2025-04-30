1649d9367bac56cc694b86e1ec961ed024da2372
Howon Lee <kck4156@kagamine-rin.com>
2025-04-30 16:34:32 +0900

    물리적 저장소 지정

--- History.AppHost/Program.cs
+++ History.AppHost/Program.cs
@@ -6,6 +6,7 @@ var password = builder.AddParameter("password", "***REMOVED***", secret: true);
 var mongodb = builder.AddMongoDB("MongoDB", 27017, username, password)
     .WithLifetime(ContainerLifetime.Persistent)
     .WithMongoExpress()
+    .WithDataBindMount("C:\\HistoryData")
     .AddDatabase("History");
 
 builder.AddProject<Projects.History_ApiService>("ApiService")
