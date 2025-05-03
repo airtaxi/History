17812dbd3a3b90033a5e47c25a933edf131f0d5a
Howon Lee <kck4156@kagamine-rin.com>
2025-05-03 14:28:57 +0900

    디스크립션 보여주게 수정

--- History.MobileClient/ViewModels/FullScreenVideoViewModel.cs
+++ History.MobileClient/ViewModels/FullScreenVideoViewModel.cs
@@ -9,7 +9,7 @@ namespace History.MobileClient.ViewModels;
 
 public partial class FullScreenVideoViewModel : VideoViewModel
 {
-    public FullScreenVideoViewModel(VideoViewModel source) : base(source.Uri)
+    public FullScreenVideoViewModel(VideoViewModel source) : base(source.Uri, source.Description)
     {
         Aspect = Aspect.AspectFit;
         VideoShouldAutoPlay = true;
