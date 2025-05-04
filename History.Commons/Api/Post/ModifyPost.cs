21a32c9960ea93ade6bbcc24890f88ca2fe427e7
Howon Lee <kck4156@kagamine-rin.com>
2025-05-05 00:05:30 +0900

    게시글 수정 시 게시글 반환하게 수정

--- History.Commons/Api/Post/ModifyPost.cs
+++ History.Commons/Api/Post/ModifyPost.cs
@@ -1,5 +1,6 @@
 ﻿using History.Commons.DataTypes.Contents;
 using History.Commons.DataTypes.RequestDtos;
+using History.Commons.DataTypes.ResponseDtos;
 using History.Commons.Enums;
 using History.Commons.Interfaces;
 using RestSharp;
@@ -11,14 +12,15 @@ using System.Threading.Tasks;
 
 namespace History.Commons.Api.Post;
 
-public class ModifyPost : IAuthRequiredRequest, IRequestWithForm, IRequestWithFiles
+public class ModifyPost : IBaseRequest<PostResponseDto>, IAuthRequiredRequest, IRequestWithForm, IRequestWithFiles, IRequestWithUrlParameters
 {
     public string Path => "/api/post/modify/{postId}";
     public Method Method => Method.Post;
     public object Body { get; set; }
     public Dictionary<string, byte[]> Files { get; set; }
+    public Dictionary<string, string> UrlParameters { get; set; } = [];
 
-    public ModifyPost(List<BaseContent> contents, DiscoveryOption discoveryOption, List<string> discoveryOptionSelectedUserIds = null, Dictionary<string, byte[]> files = null)
+    public ModifyPost(string postId, List<BaseContent> contents, DiscoveryOption discoveryOption, List<string> discoveryOptionSelectedUserIds = null, Dictionary<string, byte[]> files = null)
     {
         Body = new ModifyPostRequestDto
         {
@@ -27,5 +29,6 @@ public class ModifyPost : IAuthRequiredRequest, IRequestWithForm, IRequestWithFi
             DiscoveryOptionSelectedUserIds = discoveryOptionSelectedUserIds
         };
         Files = files ?? [];
+        UrlParameters["postId"] = postId;
     }
 }
