b4cd2be631c9cb327f19928a3385c3d27c7b261b
Howon Lee <kck4156@kagamine-rin.com>
2025-05-04 11:35:07 +0900

    핸들 변경 API 추가

---
+++ History.Commons/Api/User/UpdateHandle.cs
@@ -0,0 +1,23 @@
+﻿using History.Commons.DataTypes.RequestDtos;
+using History.Commons.DataTypes.ResponseDtos;
+using History.Commons.Interfaces;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace History.Commons.Api.User;
+
+public class UpdateHandle : IAuthRequiredRequest, IRequestWithBody
+{
+    public string Path => "/api/user/handle";
+    public Method Method => Method.Put;
+    public object Body { get; set; }
+
+    public UpdateHandle(string handle) => Body = new UpdateUserHandleRequestDto
+    {
+        Handle = handle
+    };
+}
