562ca537bfff99d84c0567b6b8807a0779c154f0
Howon Lee <kck4156@kagamine-rin.com>
2025-05-06 18:15:38 +0900

    버그 수정

--- History.MobileClient/ViewModels/MediaTemplateSelector.cs
+++ History.MobileClient/ViewModels/MediaTemplateSelector.cs
@@ -4,10 +4,11 @@ internal class MediaTemplateSelector : DataTemplateSelector
 {
     public DataTemplate VideoTemplate { get; set; }
     public DataTemplate ImageTemplate { get; set; }
+    public DataTemplate FullScreenImageTemplate { get; set; }
 
     protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
     {
-        if (item is ImageViewModel) return ImageTemplate;
+        if (item is ImageViewModel imageViewModel) return imageViewModel.IsFullScreen ? FullScreenImageTemplate : ImageTemplate;
         else if (item is VideoViewModel) return VideoTemplate;
         else throw new ArgumentException("Unknown item type", nameof(item));
     }
