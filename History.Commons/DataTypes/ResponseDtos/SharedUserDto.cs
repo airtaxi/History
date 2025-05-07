f3174bb254be5f9c7cf2971858dfd5bbfc5a5e64
Howon Lee <kck4156@kagamine-rin.com>
2025-05-07 18:01:52 +0900

    SharedUserDto 생성

---
+++ History.Commons/DataTypes/ResponseDtos/SharedUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace History.Commons.DataTypes.ResponseDtos;
+
+public class SharedUserDto
+{
+    public UserResponseDto User { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
