3f0537e366a8ba194b3b020445f6d999c5e3556d
Howon Lee <kck4156@kagamine-rin.com>
2025-05-12 19:45:44 +0900

    알림 조회 기능 추가

---
+++ History.MobileClient/Pages/NotificationsPage.xaml.cs
@@ -0,0 +1,106 @@
+using CommunityToolkit.Mvvm.Messaging;
+using History.Commons.Api.Post;
+using History.Commons.Api.User;
+using History.MobileClient.DataTypes;
+using History.MobileClient.ViewModels;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace History.MobileClient.Pages;
+
+public partial class NotificationsPage : ContentPage
+{
+    private readonly ObservableCollection<NotificationViewModel> _viewModels = [];
+    private NotificationViewModel _lastViewModel;
+    private readonly SemaphoreSlim _fetchSemaphore = new(1, 1);
+
+    public NotificationsPage()
+	{
+		InitializeComponent();
+
+        MainCollectionView.ItemsSource = _viewModels;
+        WeakReferenceMessenger.Default.Register<NotificationsMessage>(this, OnNotificationsMessage);
+    }
+
+    public async Task RefreshAsync()
+    {
+        try
+        {
+            await _fetchSemaphore.WaitAsync();
+
+            var notifications = await App.ExecuteRequestAsync(new GetNotifications());
+            if (notifications.IsSuccess) WeakReferenceMessenger.Default.Send(new NotificationsMessage(notifications.Value));
+        }
+        finally { _fetchSemaphore.Release(); }
+    }
+
+    private async Task LoadMoreAsync()
+    {
+        try
+        {
+            await _fetchSemaphore.WaitAsync();
+
+            var lastViewModel = _viewModels.OfType<NotificationViewModel>().LastOrDefault();
+            var notificationsResult = await App.ExecuteRequestAsync(new GetNotifications(lastViewModel.Notification.Id));
+            if (notificationsResult.IsSuccess)
+            {
+                var notifications = notificationsResult.Value;
+                var viewModels = notifications.Select(x => new NotificationViewModel(x));
+                _lastViewModel = viewModels.LastOrDefault();
+                foreach (var viewModel in viewModels) _viewModels.Add(viewModel);
+            }
+            else return;
+        }
+        finally { _fetchSemaphore.Release(); }
+    }
+
+    private async void OnRefreshing(object sender, EventArgs e)
+    {
+        await RefreshAsync();
+        (sender as RefreshView).IsRefreshing = false;
+    }
+
+    private View _lastView;
+    private void OnChildAdded(object sender, ElementEventArgs e)
+    {
+        var view = e.Element as View;
+        var viewModel = view.BindingContext as NotificationViewModel;
+        if (viewModel == _lastViewModel)
+        {
+            if (_lastView != null) _lastView.Loaded -= OnLastItemViewLoaded;
+            view.Loaded += OnLastItemViewLoaded;
+            _lastView = view;
+        }
+    }
+
+    private async void OnLastItemViewLoaded(object sender, EventArgs e)
+    {
+        if (_fetchSemaphore.CurrentCount > 0)
+        {
+            if (_lastView != null) _lastView.Loaded -= OnLastItemViewLoaded;
+            await LoadMoreAsync();
+        }
+    }
+
+    private bool _isInitialized = false;
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (_isInitialized) return;
+        _isInitialized = true;
+
+        await RefreshAsync();
+    }
+
+    private void OnNotificationsMessage(object recipient, NotificationsMessage message)
+    {
+        var notifications = message.Value;
+        if (notifications == null) return;
+
+        _viewModels.Clear();
+        var viewModels = notifications.Select(x => new NotificationViewModel(x));
+        _lastViewModel = viewModels.LastOrDefault();
+        foreach (var viewModel in viewModels) _viewModels.Add(viewModel);
+    }
+}
---
+++ History.MobileClient/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,71 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using History.Commons.Api.Post;
+using History.Commons.DataTypes;
+using History.Commons.DataTypes.ResponseDtos;
+using History.Commons.Enums;
+using History.MobileClient.Pages;
+using Java.Security.Acl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace History.MobileClient.ViewModels;
+
+public partial class NotificationViewModel(NotificationResponseDto notification) : ObservableObject
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Title))]
+    [NotifyPropertyChangedFor(nameof(Body))]
+    [NotifyPropertyChangedFor(nameof(IsBodyVisible))]
+    [NotifyPropertyChangedFor(nameof(TimestampText))]
+    [NotifyPropertyChangedFor(nameof(ImageMedia))]
+    [NotifyPropertyChangedFor(nameof(ProfileMedia))]
+    public partial NotificationResponseDto Notification { get; private set; } = notification;
+
+    public string Title => Notification.Title;
+    public string Body => Notification.Body;
+    public bool IsBodyVisible => !string.IsNullOrEmpty(Notification.Body);
+    public string TimestampText => Utils.GenerateFriendlyTimestamp(Notification.CreatedAt, null);
+    public ImageViewModel ImageMedia => !string.IsNullOrEmpty(Notification.ImageUrl) ? new(Notification.ImageUrl) { Aspect = Aspect.AspectFill } : null;
+    public bool IsImageVisible => !string.IsNullOrEmpty(Notification.ImageUrl) && Notification.Type != NotificationType.FriendRequest;
+    public IMediaViewModel ProfileMedia => Notification.User.UsesAnimatedProfileMedia
+        ? new VideoViewModel(Utils.GenerateMediaUri(Notification.User.ProfileMediaId))
+        : new ImageViewModel(Utils.GenerateMediaUri(Notification.User.ProfileMediaId) ?? Constants.DefaultProfileImageFileName);
+
+    [RelayCommand]
+    public async Task HandleTapAsync()
+    {
+        if(Notification.Data == null) return;
+        var type = Notification.Type;
+
+        if (type == NotificationType.FriendRequest)
+        {
+            if(!Notification.Data.TryGetValue("UserId", out var userId)) return;
+
+            var page = new UserPage(userId);
+            await App.PushModalAsync(page);
+        }
+        else
+        {
+            if (!Notification.Data.TryGetValue("PostId", out var postId)) return;
+
+            var postResult = await App.ExecuteRequestAsync(new GetPost(postId));
+            if (!postResult.IsSuccess) return;
+
+            var post = postResult.Value;
+            var viewModel = new PostViewModel(post, false);
+            var page = new PostPage(viewModel);
+            await App.PushModalAsync(page);
+        }
+    }
+
+    [RelayCommand]
+    public async Task HandleProfileTapAsync()
+    {
+        var profilePage = new UserPage(Notification.User.UserId);
+        await App.PushModalAsync(profilePage);
+    }
+}
