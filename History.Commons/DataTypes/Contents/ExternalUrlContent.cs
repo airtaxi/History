c0485e86367920a1acb088764737cd9df68f0a07
Howon Lee <kck4156@kagamine-rin.com>
2025-05-13 18:23:23 +0900

    ThumbnailUrl -> ThumbnailImageUrl

--- History.Commons/DataTypes/Contents/ExternalUrlContent.cs
+++ History.Commons/DataTypes/Contents/ExternalUrlContent.cs
@@ -4,6 +4,6 @@ public class ExternalUrlContent : BaseContent
 {
     public string Title { get; set; }
     public string Description { get; set; }
-    public string ThumbnailUrl { get; set; }
     public string SourceUrl { get; set; }
+    public string ThumbnailImageUrl { get; set; }
 }
