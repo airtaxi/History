50886f2443601be7e2db246e7db9c7c00bdce9c0
Howon Lee <kck4156@kagamine-rin.com>
2025-05-13 19:33:49 +0900

    버그 수정

--- History.Commons/DataTypes/Contents/BaseContent.cs
+++ History.Commons/DataTypes/Contents/BaseContent.cs
@@ -5,7 +5,7 @@ namespace History.Commons.DataTypes.Contents;
 
 [BsonKnownTypes(typeof(ProfileContent), typeof(ExternalUrlContent), typeof(MediaContent), typeof(UploadContent), typeof(TextContent), typeof(StickerContent))]
 [JsonDerivedType(typeof(ProfileContent), "profile")]
-[JsonDerivedType(typeof(ExternalUrlContent), "text")]
+[JsonDerivedType(typeof(ExternalUrlContent), "externalUrl")]
 [JsonDerivedType(typeof(MediaContent), "media")]
 [JsonDerivedType(typeof(UploadContent), "upload")]
 [JsonDerivedType(typeof(TextContent), "text")]
