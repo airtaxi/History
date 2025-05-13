974b40d3941935fd8b913da3070c6b757ff85034
Howon Lee <kck4156@kagamine-rin.com>
2025-05-13 20:08:46 +0900

    API 적용 및 API 수정

---
+++ History.MobileClient/ViewModels/ExternalUriContentViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using History.Commons.DataTypes.Contents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace History.MobileClient.ViewModels;
+
+public partial class ExternalUrlContentViewModel(ExternalUrlContent externalUrlContent) : ObservableObject, IContentViewModel
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Title))]
+    [NotifyPropertyChangedFor(nameof(Description))]
+    [NotifyPropertyChangedFor(nameof(Domain))]
+    [NotifyPropertyChangedFor(nameof(ThumbnailImage))]
+    public partial ExternalUrlContent ExternalUrlContent { get; set; } = externalUrlContent;
+
+    public string Title => ExternalUrlContent.Title;
+    public string Description => ExternalUrlContent.Description;
+    public string Domain => ExternalUrlContent.Domain;
+    public ImageViewModel ThumbnailImage => new(ExternalUrlContent.ThumbnailImageUrl) { Aspect = Aspect.AspectFill };
+
+    [RelayCommand]
+    public async Task HandleTapAsync()
+    {
+        var uri = new Uri(ExternalUrlContent.SourceUrl);
+        await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+    }
+}
