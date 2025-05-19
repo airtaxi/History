5dd8f12d37e1028d307e86edcace7bf2d1fe1931
Howon Lee <kck4156@kagamine-rin.com>
2025-05-19 12:35:31 +0900

    특정 친구 (비)공개 친구 선택 인터페이스 개발중

---
+++ History.MobileClient/Pages/DiscoveryOptionSelectUsersPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace History.MobileClient.Pages;
+
+public partial class DiscoveryOptionSelectUsersPage : ContentPage
+{
+	public DiscoveryOptionSelectUsersPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ History.MobileClient/ViewModels/SelectUserViewModel.cs
@@ -0,0 +1,69 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using History.Commons.DataTypes.ResponseDtos;
+using History.MobileClient.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace History.MobileClient.ViewModels
+{
+    public partial class SelectUserViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(Nickname))]
+        [NotifyPropertyChangedFor(nameof(ProfileMedia))]
+        public partial UserResponseDto User { get; set; }
+
+        public bool IsSelected
+        {
+            get => field;
+            set
+            {
+                if (field == value) return;
+
+                if (value) WeakReferenceMessenger.Default.Send(new UserSelectedMessage(User));
+                else WeakReferenceMessenger.Default.Send(new UserUnselectedMessage(User));
+
+                OnPropertyChanging(nameof(IsSelected));
+                field = value;
+                OnPropertyChanged(nameof(IsSelected));
+            }
+        }
+
+        public string Nickname => User.Nickname;
+
+        public IMediaViewModel ProfileMedia => User.UsesAnimatedProfileMedia
+        ? new VideoViewModel(Utils.GenerateMediaUri(User.ProfileMediaId))
+        : new ImageViewModel(Utils.GenerateMediaUri(User.ProfileMediaId) ?? Constants.DefaultProfileImageFileName);
+
+        public SelectUserViewModel(UserResponseDto user)
+        {
+            User = user;
+
+            WeakReferenceMessenger.Default.Register<UserSelectedMessage>(this, OnUserSelectedMessageReceived);
+
+            WeakReferenceMessenger.Default.Register<UserUnselectedMessage>(this,  OnUserUnselectedMessageReceived);
+        }
+
+        public void OnUserSelectedMessageReceived(object recipient, UserSelectedMessage message)
+        {
+            if (message.Value.UserId != User.UserId) return;
+
+            IsSelected = true;
+        }
+
+        public void OnUserUnselectedMessageReceived(object recipient, UserUnselectedMessage message)
+        {
+            if (message.Value.UserId != User.UserId) return;
+
+            IsSelected = false;
+        }
+
+        [RelayCommand]
+        public void HandleTap() => IsSelected = !IsSelected;
+    }
+}
