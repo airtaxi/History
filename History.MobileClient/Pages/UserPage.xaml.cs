c0dbf95891aa610feb73d4e8d46b00e1e28fa36a
Howon Lee <kck4156@kagamine-rin.com>
2025-05-24 12:37:50 +0900

    버그 수정 시도

--- History.MobileClient/Pages/UserPage.xaml.cs
+++ History.MobileClient/Pages/UserPage.xaml.cs
@@ -179,12 +179,12 @@ public partial class UserPage : ContentPage
     }
     private async void OnTitleLabelTapped(object sender, TappedEventArgs e)
     {
-        await RefreshAsync();
-
         var firstViewModel = _viewModels.FirstOrDefault();
         if (firstViewModel == null) return;
 
         MainCollectionView.ScrollTo(firstViewModel, null, ScrollToPosition.Start, false);
+
+        await RefreshAsync();
     }
 
     private async void OnSettingsImageTapped(object sender, TappedEventArgs e) => await DisplayAlert("안내", "제작중입니다.", "확인");
