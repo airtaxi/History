3487c15bdc92d0906545442d291335b420b3c86c
Howon Lee <kck4156@kagamine-rin.com>
2025-05-27 14:09:31 +0900

    설정 구현

---
+++ History.MobileClient/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,101 @@
+using AndroidX.Media3.Database;
+using CommunityToolkit.Mvvm.Messaging;
+using History.Commons;
+using History.Commons.Api.User;
+using History.Commons.DataTypes.ResponseDtos;
+using History.Commons.Enums;
+using History.MobileClient.DataTypes;
+
+namespace History.MobileClient.Pages;
+
+public partial class SettingsPage : ContentPage
+{
+    private bool _isInForeground;
+
+	public SettingsPage(UserResponseDto user)
+	{
+        InitializeComponent();
+
+        VersionLabel.Text = AppInfo.Current.VersionString;
+        FriendListDiscovryOptionLabel.Text = user.FriendListDiscoveryOption.ToDisplayString();
+
+        WeakReferenceMessenger.Default.Register<LoadingStateChangedMessage>(this, OnLoadingStateChangedMessageReceived);
+    }
+
+	private async void OnBackImageTapped(object sender, TappedEventArgs e) => await App.PopModalAsync();
+
+    private async void OnLogoutGridTapped(object sender, TappedEventArgs e)
+    {
+        var result = await DisplayAlert("안내", "정말로 로그아웃을 하시겠습니까?", "네", "아니오");
+        if (!result) return;
+
+        Configuration.SetValue("AccessToken", null);
+        Configuration.SetValue("RefreshToken", null);
+        Shared.ApiHandler = ApiHandler.Public;
+        Shared.UserId = default;
+        Shared.MyRank = default;
+        Shared.LastUsedPostDiscoveryOption = default;
+        Shared.Friends = default;
+
+        App.Page = new LoginPage();
+    }
+
+    private async void OnWithdrawGridTapped(object sender, TappedEventArgs e)
+    {
+        var result = await DisplayAlert("안내", "정말로 회원 탈퇴를 하시겠습니까?", "네", "아니오");
+        if (!result) return;
+
+        result = await DisplayAlert("경고", "회원 탈퇴는 되돌릴 수 없습니다. 정말로 회원 탈퇴를 하시겠습니까?", "네", "아니오");
+        if (!result) return;
+
+        result = await DisplayAlert("경고", "회원 탈퇴를 하시면 이용 약관에 따라 유예 기간 없이 모든 모든 데이터가 삭제됩니다. 이에 동의하십니까?", "네", "아니오");
+        if (!result) return;
+
+        var response = await App.ExecuteRequestAsync(new Withdraw());
+        if (response.IsSuccess)
+        {
+            await DisplayAlert("안내", "회원 탈퇴가 완료되었습니다. 이용해 주셔서 감사합니다.", "확인");
+            App.Page = new LoginPage();
+        }
+    }
+
+    private async void OnFriendListDiscovryOptionGridTapped(object sender, TappedEventArgs e)
+    {
+        var discoveryOptions = Enum.GetValues<DiscoveryOption>().ToList();
+        discoveryOptions.Remove(DiscoveryOption.SelectedUsers);
+        discoveryOptions.Remove(DiscoveryOption.UnselectedUsers);
+
+        var rawDiscoveryOptions = discoveryOptions.Select(x => x.ToDisplayString()).ToArray();
+        var rawDiscoveryOption = await App.Page.DisplayActionSheet("친구 목록 공개 범위 설정", Constants.PromptCancel, null, rawDiscoveryOptions);
+
+        if (rawDiscoveryOption == null || rawDiscoveryOption == Constants.PromptCancel) return;
+
+        var discoveryOption = DiscoveryOptionExtensions.FromDisplayString(rawDiscoveryOption);
+        var result = await App.ExecuteRequestAsync(new UpdateFriendListDiscoveryOption(discoveryOption));
+        if (result.IsSuccess) FriendListDiscovryOptionLabel.Text = rawDiscoveryOption;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        _isInForeground = true;
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        _isInForeground = false;
+    }
+
+    private void OnLoadingStateChangedMessageReceived(object recipient, LoadingStateChangedMessage message)
+    {
+        if (!_isInForeground) return;
+
+        Dispatcher.Dispatch(() =>
+        {
+            var isLoading = message.Value;
+            MainActivityIndicator.IsRunning = isLoading;
+            IsEnabled = !isLoading;
+        });
+    }
+}
