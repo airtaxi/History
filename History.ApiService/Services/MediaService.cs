0ccfb01674dcdd123146addbcd4410af773a0dc4
Howon Lee <kck4156@kagamine-rin.com>
2025-05-29 16:37:33 +0900

    이미지 업로드 사이즈 제한 15MB -> 25MB로 상향 (비디오는 해당사항 없음)

--- History.ApiService/Services/MediaService.cs
+++ History.ApiService/Services/MediaService.cs
@@ -97,7 +97,7 @@ public class MediaService(IMongoDatabase database) : IMediaService
                     }
 
                     // Check file size
-                    var isOverSize = bytes.Length > CommonsConstants.MaxUploadFileSize; // 15MB
+                    var isOverSize = bytes.Length > (wasImage ? CommonsConstants.MaxImageUploadFileSize : CommonsConstants.MaxUploadFileSize); // 25, 15 MB for images and videos respectively
                     if (isOverSize)
                     {
                         return (Index: index, Result: Result.Failure(ErrorType.BadRequest, $"{file.FileName}: 파일 크기가 너무 큽니다."), MediaContent: null);
