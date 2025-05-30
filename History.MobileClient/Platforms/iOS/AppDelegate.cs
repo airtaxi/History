959177d0ef63043919f704b6659d24649aa0763f
Howon Lee <kck4156@kagamine-rin.com>
2025-05-30 13:30:51 +0900

    키보드 사이즈 메시지 구현

--- History.MobileClient/Platforms/iOS/AppDelegate.cs
+++ History.MobileClient/Platforms/iOS/AppDelegate.cs
@@ -1,5 +1,7 @@
-﻿using Foundation;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Foundation;
 using Google.SignIn;
+using History.MobileClient.DataTypes;
 using UIKit;
 
 namespace History.MobileClient;
@@ -11,6 +13,41 @@ public class AppDelegate : MauiUIApplicationDelegate
     {
         SignIn.SharedInstance.HandleUrl(url);
         return true;
+    }
+
+    public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+    {
+        var result = base.FinishedLaunching(app, options);
+
+        NSNotificationCenter.DefaultCenter.AddObserver(
+            UIKeyboard.WillShowNotification,
+            OnKeyboardWillShow);
+
+        NSNotificationCenter.DefaultCenter.AddObserver(
+            UIKeyboard.WillHideNotification,
+            OnKeyboardWillHide);
+
+        return result;
+    }
+
+    private void OnKeyboardWillShow(NSNotification notification)
+    {
+        if (notification.UserInfo != null)
+        {
+            var keyboardFrame = ((NSValue)notification.UserInfo[UIKeyboard.FrameEndUserInfoKey]).CGRectValue;
+            var keyboardHeight = keyboardFrame.Height;
+
+            WeakReferenceMessenger.Default.Send(new KeyboardSizeMessage(keyboardHeight));
+        }
+    }
+
+    private void OnKeyboardWillHide(NSNotification notification) => WeakReferenceMessenger.Default.Send(new KeyboardSizeMessage(0));
+
+    public override void WillTerminate(UIApplication application)
+    {
+        // 알림 구독 해제
+        NSNotificationCenter.DefaultCenter.RemoveObserver(this);
+        base.WillTerminate(application);
     }
 
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
