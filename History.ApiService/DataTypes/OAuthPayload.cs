b9b06d4b048ed72bce18a3ac7b998e6ee981f7ce
Howon Lee <kck4156@kagamine-rin.com>
2025-06-01 02:36:54 +0900

    애플 로그인 검증 구현

---
+++ History.ApiService/DataTypes/OAuthPayload.cs
@@ -0,0 +1,8 @@
+﻿namespace History.ApiService.DataTypes;
+
+public class OAuthPayload
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
