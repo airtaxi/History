3bf76d36e3544d411f1e646bf15eab701ec4e86b
Howon Lee <kck4156@kagamine-rin.com>
2025-06-02 21:49:48 +0900

    API 앤드포인트 매칭

---
+++ History.Commons/Api/Report/ProcessReportRecord.cs
@@ -0,0 +1,18 @@
+﻿using History.Commons.Interfaces;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace History.Commons.Api.Report;
+
+public class ProcessReportRecord : IAuthRequiredRequest, IRequestWithUrlParameters
+{
+    public string Path => "/api/report/process/{recordId}";
+    public Method Method => Method.Post;
+    public Dictionary<string, string> UrlParameters { get; set; } = [];
+
+    public ProcessReportRecord(string recordId) => UrlParameters["recordId"] = recordId;
+}
