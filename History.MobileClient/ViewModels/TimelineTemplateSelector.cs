cb56b48ca05834a4456d3d06ed59816b8faabaf8
Howon Lee <kck4156@kagamine-rin.com>
2025-06-14 13:10:03 +0900

    타임라인 레이아웃 버그 수정

--- History.MobileClient/ViewModels/TimelineTemplateSelector.cs
+++ History.MobileClient/ViewModels/TimelineTemplateSelector.cs
@@ -3,18 +3,13 @@
 internal class TimelineTemplateSelector : DataTemplateSelector
 {
     public DataTemplate ProfileTemplate { get; set; }
-    public DataTemplate ApplePostTemplate { get; set; }
     public DataTemplate PostTemplate { get; set; }
     public DataTemplate RepostTemplate { get; set; }
 
     protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
     {
         if (item is RepostViewModel) return RepostTemplate;
-#if IOS
-        else if (item is PostViewModel) return ApplePostTemplate;
-#else
         else if (item is PostViewModel) return PostTemplate;
-#endif
         else if (item is ProfileViewModel) return ProfileTemplate;
         else throw new ArgumentException("Unknown item type", nameof(item));
     }
