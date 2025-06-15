a0c32553c8605447989da56330e33f6d822d9ab1
Howon Lee <kck4156@kagamine-rin.com>
2025-06-15 14:30:50 +0900

    버그 수정

---
+++ History.MobileClient/ViewModels/PublicPostViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using History.Commons.DataTypes.ResponseDtos;
+using History.MobileClient.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace History.MobileClient.ViewModels;
+
+public partial class PublicPostViewModel(PostResponseDto post) : PostViewModel(post, false);
