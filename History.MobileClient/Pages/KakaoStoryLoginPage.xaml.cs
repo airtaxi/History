6d6adb6e355cc673ac360c61b4f35366e42e76a9
Howon Lee <kck4156@kagamine-rin.com>
2025-06-15 21:19:51 +0900

    로그인 오류 수정 및 설정에 재로그인 메뉴 추가

--- History.MobileClient/Pages/KakaoStoryLoginPage.xaml.cs
+++ History.MobileClient/Pages/KakaoStoryLoginPage.xaml.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using History.MobileClient.Helpers;
+using History.MobileClient.KakaoStory;
 
 namespace History.MobileClient.Pages;
 
@@ -14,7 +15,7 @@ public partial class KakaoStoryLoginPage : ContentPage
     protected override void OnAppearing()
     {
         base.OnAppearing();
-        BrowserWebView.Source = "https://accounts.kakao.com/login/?continue=https%3A%2F%2Fstory.kakao.com%2F&talk_login=&login_type=simple#login";
+        BrowserWebView.Source = "https://accounts.kakao.com/logout?continue=https://story.kakao.com/";
     }
 
     protected override void OnDisappearing()
@@ -44,8 +45,18 @@ public partial class KakaoStoryLoginPage : ContentPage
         bool isSuccess = cookies.Any(x => x.Name == "_karmt");
         if (isSuccess)
         {
-            var setResult = _taskCompletionSource.TrySetResult(cookies);
-            if (setResult) await App.PopModalAsync();
+            var cookieContainer = new CookieContainer();
+            foreach (var cookie in cookies) cookieContainer.Add(cookie);
+
+            KakaoStoryApiHandler.Init(cookieContainer, cookies, null);
+            try
+            {
+                var friends = await KakaoStoryApiHandler.GetFriends();
+                var setResult = _taskCompletionSource.TrySetResult(cookies);
+                if (setResult) await App.PopModalAsync();
+            }
+            catch { }
+
         }
     }
 
