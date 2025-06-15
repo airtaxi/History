039d609840fc5249c3efa33285cd3dcc5d361f4f
Howon Lee <kck4156@kagamine-rin.com>
2025-06-15 15:20:21 +0900

    히스토리 통하여 작성되었다는 메시지에 구분선 추가

--- History.MobileClient/Pages/EditPostPage.xaml.cs
+++ History.MobileClient/Pages/EditPostPage.xaml.cs
@@ -446,7 +446,7 @@ public partial class EditPostPage : ContentPage
                             try
                             {
                                 var text = MainTextContent.MentionEditor.Text;
-                                text += $"\n\n\n이 게시글은 대체 SNS인 히스토리를 통하여 작성되었습니다.\n히스토리 디스코드 채널에 참여하여 히스토리 베타에 참여해보세요.\n베타 참여 디스코드: {Constants.DiscordInviteUrl}";
+                                text += $"\n----------------------\n이 게시글은 대체 SNS인 히스토리를 통하여 작성되었습니다.\n히스토리 디스코드 채널에 참여하여 히스토리 베타에 참여해보세요.\n베타 참여 디스코드: {Constants.DiscordInviteUrl}";
                                 text = text.Trim();
                                 var quoteDatas = KakaoStoryUtils.GetQuoteDataFromString(text);
 
