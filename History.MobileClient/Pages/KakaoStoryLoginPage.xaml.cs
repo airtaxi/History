8107db888af7cebec14572688f4a4b7c9012cba8
Howon Lee <kck4156@kagamine-rin.com>
2025-06-16 14:01:35 +0900

    타이머 추가

--- History.MobileClient/Pages/KakaoStoryLoginPage.xaml.cs
+++ History.MobileClient/Pages/KakaoStoryLoginPage.xaml.cs
@@ -13,9 +13,19 @@ public partial class KakaoStoryLoginPage : ContentPage
 
     public Task<List<Cookie>> GetResultAsync() => _taskCompletionSource.Task;
 
+    private System.Timers.Timer _timer;
+
     protected override void OnAppearing()
     {
         base.OnAppearing();
+
+        if(_timer == null)
+        {
+            _timer = new(1250);
+            _timer.Elapsed += (s, e) => Dispatcher.Dispatch(async () => await CheckCookies());
+            _timer.Start();
+        }
+
         BrowserWebView.Source = "https://accounts.kakao.com/logout?continue=https%3A%2F%2Faccounts.kakao.com%2Flogin%2F%3Fcontinue%3Dhttps%253A%252F%252Fstory.kakao.com";
         Toast.Make("간편 로그인은 지원하지 않습니다. 계정을 입력하여 로그인해주세요").Show();
     }
@@ -27,6 +37,13 @@ public partial class KakaoStoryLoginPage : ContentPage
         if (!_taskCompletionSource.Task.IsCompleted)
         {
             _taskCompletionSource.TrySetResult(null);
+        }
+
+        if (_timer != null)
+        {
+            _timer.Stop();
+            _timer.Dispose();
+            _timer = null;
         }
     }
 
@@ -41,6 +58,11 @@ public partial class KakaoStoryLoginPage : ContentPage
         MainActivityIndicator.IsVisible = false;
         MainActivityIndicator.IsRunning = false;
 
+        await CheckCookies();
+    }
+
+    private async Task CheckCookies()
+    {
         var cookies = await WebViewCookieHelper.GetCookieListAsync(BrowserWebView, "https://story.kakao.com");
         if (cookies == null) return;
 
