cd4b4da12c0366f50a6573b12da7c8fa61a9d0be
Howon Lee <kck4156@kagamine-rin.com>
2025-06-17 17:31:28 +0900

    리펙토링

--- History.ApiService/Controllers/GoogleController.cs
+++ History.ApiService/Controllers/GoogleController.cs
@@ -12,23 +12,15 @@ public class GoogleController : ControllerBase
 {
     private const string ClientId = "401981104412-7n578mga4lggbspntkgg7gtikoqq3auk.apps.googleusercontent.com";
     private const string ClientSecret = "***REMOVED***"; // Replace with your actual client secret
+    private const string AuthorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+    private const string RedirectUri = "https://api.history.cenox.io/api/auth/google/callback";
 
     [HttpGet("login")]
-    public IActionResult Login([FromQuery] string redirectUrl)
-    {
-        string clientId = "401981104412-7n578mga4lggbspntkgg7gtikoqq3auk.apps.googleusercontent.com";
-        string redirectUri = "https://api.history.cenox.io/api/auth/google/callback";
-
-        string authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
-
-        var url = $"{authorizationEndpoint}?response_type=code" +
-                  $"&client_id={HttpUtility.UrlEncode(clientId)}" +
-                  $"&redirect_uri={HttpUtility.UrlEncode(redirectUri)}" +
-                  $"&scope={HttpUtility.UrlEncode("openid email profile")}" +
-                  $"&state={HttpUtility.UrlEncode(redirectUrl)}";
-
-        return Redirect(url);
-    }
+    public IActionResult Login([FromQuery] string redirectUrl) => Redirect($"{AuthorizationEndpoint}?response_type=code" +
+        $"&client_id={HttpUtility.UrlEncode(ClientId)}" +
+        $"&redirect_uri={HttpUtility.UrlEncode(RedirectUri)}" +
+        $"&scope={HttpUtility.UrlEncode("openid email profile")}" +
+        $"&state={HttpUtility.UrlEncode(redirectUrl)}");
 
     [HttpGet("callback")]
     public async Task<IActionResult> Callback([FromQuery] string code, [FromQuery] string state)
@@ -36,17 +28,15 @@ public class GoogleController : ControllerBase
         if (string.IsNullOrEmpty(code))
             return RedirectToAction("Login", "Google");
 
-        string redirectUri = "https://api.history.cenox.io/api/auth/google/callback";
-
         using var httpClient = new HttpClient();
         var tokenRequestParams = new Dictionary<string, string>
-            {
-                { "code", code },
-                { "client_id", ClientId },
-                { "client_secret", ClientSecret },
-                { "redirect_uri", redirectUri },
-                { "grant_type", "authorization_code" }
-            };
+        {
+            { "code", code },
+            { "client_id", ClientId },
+            { "client_secret", ClientSecret },
+            { "redirect_uri", RedirectUri },
+            { "grant_type", "authorization_code" }
+        };
 
         var tokenRequest = new HttpRequestMessage(HttpMethod.Post, "https://oauth2.googleapis.com/token")
         {
