ff86f0e01f4f62f7079243e5bdfbbedc5c2dc9a0
Howon Lee <kck4156@kagamine-rin.com>
2025-06-18 22:21:46 +0900

    스택트레이스 및 메시지 출력

--- History.MobileClient/Platforms/iOS/Program.cs
+++ History.MobileClient/Platforms/iOS/Program.cs
@@ -1,4 +1,5 @@
-﻿using UIKit;
+﻿using System.Diagnostics;
+using UIKit;
 
 namespace History.MobileClient;
 
@@ -9,6 +10,12 @@ public class Program
     {
         // if you want to use a different Application Delegate class from "AppDelegate"
         // you can specify it here.
-        UIApplication.Main(args, null, typeof(AppDelegate));
+        try { UIApplication.Main(args, null, typeof(AppDelegate)); }
+        catch (Exception exception)
+        {
+            var message = exception.Message ?? "Unknown error";
+            var stackTrace = exception.StackTrace ?? "No stack trace available";
+            Debug.WriteLine($"Exception in Main: {message} / {stackTrace}");
+        }
     }
 }
