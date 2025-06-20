8391157778d922d0341472967d903833225349d7
Howon Lee <kck4156@kagamine-rin.com>
2025-06-20 18:37:27 +0900

    대체 SNS 메시지 제거

--- History.MobileClient/Pages/EditPostPage.xaml.cs
+++ History.MobileClient/Pages/EditPostPage.xaml.cs
@@ -431,8 +431,8 @@ public partial class EditPostPage : ContentPage
                             // Samsung pass will overwrite the text content, fetch the text content before logging in to KakaoStory
                             var text = MainTextContent.MentionEditor.Text;
                             text = text.Trim();
-                            if (!string.IsNullOrWhiteSpace(text)) text += "\n----------------------\n";
-                            text += $"이 게시글은 대체 SNS인 히스토리를 통하여 작성되었습니다.\n히스토리 디스코드 채널에 참여하여 히스토리 베타에 참여해보세요.\n베타 참여 디스코드: {Constants.DiscordInviteUrl}";
+                            //if (!string.IsNullOrWhiteSpace(text)) text += "\n----------------------\n";
+                            //text += $"이 게시글은 대체 SNS인 히스토리를 통하여 작성되었습니다.\n히스토리 디스코드 채널에 참여하여 히스토리 베타에 참여해보세요.\n베타 참여 디스코드: {Constants.DiscordInviteUrl}";
 
                             bool loginNeeded = true;
                             var cookies = Configuration.GetValue<List<Cookie>>("KakaoStoryCookies");
