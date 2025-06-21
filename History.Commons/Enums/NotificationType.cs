5d61feaa156c75c4e86b0a5703b1e623d9a048ae
Howon Lee <kck4156@kagamine-rin.com>
2025-06-21 23:30:02 +0900

    오류 수정

--- History.Commons/Enums/NotificationType.cs
+++ History.Commons/Enums/NotificationType.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace History.Commons.Enums;
 
@@ -21,6 +16,5 @@ public enum NotificationType
     FavoriteFriendNewPost,
     Birthday,
     Restriction,
-    Report,
-    FavoriteFriendNowPost
+    Report
 }
