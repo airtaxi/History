8a378ea82ae0d8ff1ef690f81d2f6650ddb7ec6b
IDKonly <youde1230@gmail.com>
2025-06-30 00:54:30 +0900

    header 통일

--- History.WebFront/src/App.vue
+++ History.WebFront/src/App.vue
@@ -3,11 +3,13 @@ import { RouterView } from 'vue-router';
 import { useUiStore } from '@/stores/ui';
 import PostModal from '@/components/PostModal.vue';
 import "./App.css"
+import TheHeader from '@/components/layout/TheHeader.vue';
 
 
 const uiStore = useUiStore();
 </script>
 <template>
+  <TheHeader />
   <div class="app-container">
     <main class="main-content">
       <RouterView />
