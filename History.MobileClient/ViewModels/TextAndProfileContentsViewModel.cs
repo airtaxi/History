8408917540a73f64fd37f6315284cbeb326d40d4
Howon Lee <kck4156@kagamine-rin.com>
2025-07-02 17:10:43 +0900

    타임라인에서 더보기.. 복사되는 오류 수정

--- History.MobileClient/ViewModels/TextAndProfileContentsViewModel.cs
+++ History.MobileClient/ViewModels/TextAndProfileContentsViewModel.cs
@@ -5,5 +5,6 @@ namespace History.MobileClient.ViewModels;
 
 public class TextAndProfileContentsViewModel(List<BaseContent> textAndProfileContents, PostType postType, bool hasMedias) : IContentViewModel
 {
+    public List<BaseContent> TextAndProfileContents { get; } = textAndProfileContents;
     public FormattedString FormattedString { get; set; } = Utils.GenerateSpanFromTextAndProfileContents(textAndProfileContents, postType, hasMedias);
 }
