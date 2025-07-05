860a8354c825e7258a2a62fbb5b58ee21ae77e4a
Howon Lee <kck4156@kagamine-rin.com>
2025-07-06 05:58:31 +0900

    해시태그 탭 하는 경우 해당 해시태그로 새 글 작성

--- History.MobileClient/Resources/Styles/Post.xaml.cs
+++ History.MobileClient/Resources/Styles/Post.xaml.cs
@@ -1,4 +1,4 @@
-using History.MobileClient.ContentViews;
+﻿using History.MobileClient.ContentViews;
 using History.MobileClient.Pages;
 using History.MobileClient.ViewModels;
 
@@ -9,5 +9,15 @@ public partial class Post : ResourceDictionary
 	public Post()
 	{
 		InitializeComponent();
-	}
+    }
+
+    private async void OnHashtagBorderTapped(object sender, TappedEventArgs e)
+    {
+        var border = sender as Border;
+        var hashtag = border?.BindingContext as string;
+        if (string.IsNullOrEmpty(hashtag)) return;
+
+        var page = new EditPostPage([hashtag]);
+        await App.PushAsync(page);
+    }
 }
--- History.MobileClient/Resources/Styles/SharedPost.xaml.cs
+++ History.MobileClient/Resources/Styles/SharedPost.xaml.cs
@@ -1,9 +1,21 @@
-﻿namespace History.MobileClient.Resources.Styles;
+﻿using History.MobileClient.Pages;
+
+namespace History.MobileClient.Resources.Styles;
 
 public partial class SharedPost : ResourceDictionary
 {
     public SharedPost()
     {
         InitializeComponent();
+    }
+
+    private async void OnHashtagBorderTapped(object sender, TappedEventArgs e)
+    {
+        var border = sender as Border;
+        var hashtag = border?.BindingContext as string;
+        if (string.IsNullOrEmpty(hashtag)) return;
+
+        var page = new EditPostPage([hashtag]);
+        await App.PushAsync(page);
     }
 }
