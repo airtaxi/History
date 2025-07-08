f7295b78305ff861c713095eed845ad544766e9f
Howon Lee <kck4156@kagamine-rin.com>
2025-07-08 16:25:14 +0900

    버그 수정

--- History.MobileClient/ContentViews/SquareView.cs
+++ History.MobileClient/ContentViews/SquareView.cs
@@ -2,17 +2,13 @@
 
 public class SquareView : ContentView
 {
-	public SquareView()
-	{
-		SizeChanged += OnSizeChanged;
-        Unloaded += OnUnloaded;
-	}
-
-    private void OnUnloaded(object sender, EventArgs e)
-    {
-        Unloaded -= OnUnloaded;
-        SizeChanged -= OnSizeChanged;
-    }
+    public SquareView() => SizeChanged += OnSizeChanged;
 
     private void OnSizeChanged(object sender, EventArgs e) => HeightRequest = Width;
+
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        base.OnSizeAllocated(width, height);
+        HeightRequest = Width;
+    }
 }
