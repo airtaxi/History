995affe9334cbcef88c448fa5e85eac8c213ff6e
Howon Lee <kck4156@kagamine-rin.com>
2026-02-07 18:02:18 +0900

    오류 수정

--- History.AppHost/Program.cs
+++ History.AppHost/Program.cs
@@ -9,12 +9,13 @@ var mongodb = builder.AddMongoDB("MongoDB", 27017, username, password)
     .WithDataBindMount("C:\\HistoryData")
     .AddDatabase("History");
 
-var api = builder.AddProject<Projects.History_ApiService>("ApiService")
+var api = builder.AddDockerfile("ApiService", "../", "History.ApiService/Dockerfile")
     .WithReference(mongodb)
     .WaitFor(mongodb)
-    .WithDockerfile("../History.ApiService");
+    .WithContainerRuntimeArgs("-p", "31227:31227");
 
 builder.AddDockerfile("vue", "../History.WebFront")
+    .WithLifetime(ContainerLifetime.Persistent)
     .WaitFor(api)
     .WithContainerRuntimeArgs("-p", "5173:80");
 
