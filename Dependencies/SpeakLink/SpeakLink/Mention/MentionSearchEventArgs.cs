7f54074ef55a8c8a2a8a623fd5c516c809ec07fa
Howon Lee <kck4156@kagamine-rin.com>
2026-02-27 14:23:37 +0900

    디펜던시 삭제

--- Dependencies/SpeakLink/SpeakLink/Mention/MentionSearchEventArgs.cs
+++
@@ -1,7 +0,0 @@
-namespace SpeakLink.Mention;
-
-public class MentionSearchEventArgs(string controlCharacter, string mentionQuery) : EventArgs
-{
-    public string ControlCharacter { get; set; } = controlCharacter;
-    public string MentionQuery { get; set; } = mentionQuery;
-}
