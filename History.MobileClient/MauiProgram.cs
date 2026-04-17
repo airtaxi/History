dd029449de1320028f78c552eb007fa8845f594d
Howon Lee <kck4156@kagamine-rin.com>
2026-04-17 10:29:46 +0900

    라이센스 업데이트

--- History.MobileClient/MauiProgram.cs
+++ History.MobileClient/MauiProgram.cs
@@ -38,7 +38,7 @@ public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
-        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JHaF5cWWdCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdlWXtfdnZURmldUEN+XUJWYEo=");
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JHaF5cWWdCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdlWXtfdnZURGZfUkZ3V0pWYEo=");
 
         ApiHandler.ApplicationVersion = AppInfo.Current.VersionString;
         ApiHandler.Platform = DeviceInfo.Platform.ToString();
