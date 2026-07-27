add2b18d42d4bbe1e1c1daadc8c5c58f810264e4
Howon Lee <kck4156@kagamine-rin.com>
2026-07-27 12:48:23 +0900

    Revert "타임라인 Measure 오버헤드 시각화"
    This reverts commit f1e53599c7a677192701975e9998e00e99bca314.

--- History.MobileClient/DataTypes/PostCellMeasureInvalidationMessage.cs
+++
@@ -1,5 +0,0 @@
-using CommunityToolkit.Mvvm.Messaging.Messages;
-
-namespace History.MobileClient.DataTypes;
-
-public class PostCellMeasureInvalidationMessage(string value) : ValueChangedMessage<string>(value);
