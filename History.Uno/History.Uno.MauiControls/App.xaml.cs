d715f534f8ba74cd7ed0c2f15a939bb3354707e8
Howon Lee <kck4156@kagamine-rin.com>
2026-07-30 17:39:36 +0900

    Uno 재정비

---
+++ History.Uno/History.Uno.MauiControls/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace History.Uno.MauiControls;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+}
---
+++ History.Uno/History.Uno/Models/AppConfig.cs
@@ -0,0 +1,6 @@
+namespace History.Uno.Models;
+
+public record AppConfig
+{
+    public string? Environment { get; init; }
+}
