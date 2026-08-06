ca42b47b7dd3bab074d9ca99f925dfc9d71ab86f
Howon Lee <kck4156@kagamine-rin.com>
2026-08-07 01:04:03 +0900

    빠진 인터렉션 구현 추가

--- History.MobileClient/ViewModels/KakaoInteractionViewModel.cs
+++ History.MobileClient/ViewModels/KakaoInteractionViewModel.cs
@@ -1,23 +1,40 @@
 ﻿using History.MobileClient.Enums;
 using History.MobileClient.KakaoStory;
+using UraniumUI.Icons.MaterialSymbols;
 using static History.MobileClient.KakaoStory.KakaoStoryApiHandler.DataType;
 
 namespace History.MobileClient.ViewModels;
 
 public partial class KakaoInteractionViewModel : BaseInteractionViewModel
 {
-    public KakaoInteractionViewModel(ShareData.Share share)
+    public KakaoInteractionViewModel(ShareData.Share share, InteractionType type = InteractionType.Reaction)
     {
-        Type = InteractionType.Reaction;
+        Type = type;
         CreatedAt = share.created_at;
         TargetPostId = null;
         ReactionType = null;
 
         ProfileMedia = share.actor?.profile_image_url != null ? new ImageViewModel(share.actor.profile_image_url) : null;
-        FontFamily = "FASolid";
-        var visual = KakaoStoryUtils.GetEmotionVisual(share.emotion);
-        Glyph = visual.Glyph;
-        Color = visual.Color;
+
+        if (type == InteractionType.Share)
+        {
+            FontFamily = "MaterialSharp";
+            Glyph = MaterialSharp.Share;
+            Color = Color.FromRgb(0x65, 0x52, 0xdf);
+        }
+        else if (type == InteractionType.Repost)
+        {
+            FontFamily = "MaterialSharp";
+            Glyph = MaterialSharp.Shift_lock;
+            Color = Color.FromRgb(0x99, 0x99, 0x99);
+        }
+        else
+        {
+            FontFamily = "FASolid";
+            var visual = KakaoStoryUtils.GetEmotionVisual(share.emotion);
+            Glyph = visual.Glyph;
+            Color = visual.Color;
+        }
     }
 
     public override async Task HandleTapAsync() { }
