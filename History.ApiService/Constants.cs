b8f130c69de7e85e2ea4b49bac262a71ef4f3dc3
Howon Lee <kck4156@kagamine-rin.com>
2026-08-09 17:25:11 +0900

    신규 가입자 자동 친구 추가

--- History.ApiService/Constants.cs
+++ History.ApiService/Constants.cs
@@ -5,4 +5,5 @@ public class Constants
     public const string JwtIssuer = "History";
     public const string JwtAudience = "https://api.history.cenox.io";
     public const string JwtKey = "***REMOVED***";
+    public const string AutoFriendUserId = "106735740295566028473";
 }
