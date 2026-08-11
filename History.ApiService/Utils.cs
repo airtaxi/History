72b074311d5d4b96f943db9b0c14eb7c38cacba1
Howon Lee <kck4156@kagamine-rin.com>
2026-08-11 13:43:47 +0900

    하이퍼링크 임시 해제 (Android 출시 이후 revert)

--- History.ApiService/Utils.cs
+++ History.ApiService/Utils.cs
@@ -83,6 +83,8 @@ public static partial class Utils
 
     public static List<BaseContent> ConvertUrlsToHyperlinkContents(IEnumerable<BaseContent> contents)
     {
+        return [.. contents];
+
         var converted = new List<BaseContent>();
         foreach (var content in contents)
         {
