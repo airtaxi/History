72dc72163f47fbc017368b45a9e2f87e17e21dd5
Howon Lee <kck4156@kagamine-rin.com>
2026-08-15 17:41:10 +0900

    큰 용량의 이미지를 고른 경우 UI 최적화 개선

--- History.MobileClient/DataTypes/MediaFile.cs
+++ History.MobileClient/DataTypes/MediaFile.cs
@@ -10,12 +10,17 @@ public class MediaFile
 {
     public string FileName { get; }
     public byte[] Bytes { get; }
+    public string FilePath { get; set; }
+    public long Size { get; set; }
 
     public MediaFile(string fileName, byte[] bytes)
     {
         if (string.IsNullOrEmpty(fileName)) throw new ArgumentException("fileName cannot be null or empty.", nameof(fileName));
 
         FileName = fileName;
-        Bytes = bytes ?? throw new ArgumentNullException(nameof(bytes));
+        Bytes = bytes;
+        Size = bytes?.Length ?? 0;
     }
+
+    public byte[] GetBytes() => Bytes ?? (FilePath != null && File.Exists(FilePath) ? File.ReadAllBytes(FilePath) : []);
 }
