ebb0bb55f631bce84cce3f9cbc7a1172c19db33b
Howon Lee <kck4156@kagamine-rin.com>
2026-08-20 00:34:24 +0900

    nullable 경고 제거 (object? -> object)

--- History.MobileClient/Pages/BlazorPublicPostsPage.xaml.cs
+++ History.MobileClient/Pages/BlazorPublicPostsPage.xaml.cs
@@ -1,4 +1,4 @@
-using CommunityToolkit.Mvvm.Messaging;
+﻿using CommunityToolkit.Mvvm.Messaging;
 using History.MobileClient.Components.Timeline;
 using History.MobileClient.Helpers;
 using History.MobileClient.Messages;
@@ -22,7 +22,7 @@ public partial class BlazorPublicPostsPage : ContentPage
         {
             Selector = "#app",
             ComponentType = typeof(PublicPosts),
-            Parameters = new Dictionary<string, object?>
+            Parameters = new Dictionary<string, object>
             {
                 [nameof(PublicPosts.ViewModel)] = _viewModel
             }
